14edd2d778a6642df8bdea2a0e94405caffc178f
carellihoula <carellihoula@gmail.com>
2024-06-22 17:03:15 +0200

    Added functionality to select and display performance evolution of a specific team over multiple seasons.

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import plotly.express as px
 from src.data_processing import (load_config, create_spark_session, load_data, preprocess_data, 
                                  filter_bundesliga, aggregate_team_stats, calculate_additional_metrics, 
                                  get_best_teams)
@@ -31,16 +32,27 @@ df_best_team, df_top_three = get_best_teams(df_ranked)
 best_team_figures = plot_best_teams(df_best_team)
 top_three_figures = plot_top_three_teams(df_top_three)
 
-# Streamlit UI
-st.title("Bundesliga Team Performance Analysis")
+# Convert DataFrames Spark to Pandas
+df_ranked_pd = df_ranked.toPandas()
+df_top_three_pd = df_top_three.toPandas()
 
-# Tabs
-tab1, tab2 = st.tabs(["Best Teams by Season", "Top 3 Teams by Season"])
+#Select a team to view its evolution over the seasons
+teams = df_ranked_pd["Team"].unique()
+selected_team = st.selectbox("select a team", teams)
 
-with tab1:
-    for fig in best_team_figures:
-        st.plotly_chart(fig)
+# Filter data for selected team
+team_data = df_ranked_pd[df_ranked_pd['Team'] == selected_team]
 
-with tab2:
-    for fig in top_three_figures:
-        st.plotly_chart(fig)
+# Plot the performance of the selected team over multiple seasons
+fig_team = px.line(team_data, x='Season', y='winPercentage', title=f'Performance of {selected_team} over the seasons', labels={'winPercentage': 'Win Percentage', 'Season': 'Season'})
+st.plotly_chart(fig_team)
+
+# Plot the curves of the top three teams in the latest season
+latest_season = df_top_three_pd['Season'].max()
+top_three_latest_season = df_top_three_pd[df_top_three_pd['Season'] == latest_season]
+
+fig_top_three = px.line(top_three_latest_season, x='Season', y='winPercentage', color='Team', title=f'Top 3 teams of the {latest_season} season', labels={'winPercentage': 'Win Percentage', 'Season': 'Season'})
+st.plotly_chart(fig_top_three)
+   
+   
+
